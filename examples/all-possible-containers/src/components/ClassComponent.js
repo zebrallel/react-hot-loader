5a1d04c5185d4678c308e224f658a732a7cd7d12
xuejian.xu <412383619@qq.com>
2019-11-13 00:33:19 +0800

    update comment

--- examples/all-possible-containers/src/components/ClassComponent.js
+++ examples/all-possible-containers/src/components/ClassComponent.js
@@ -5,7 +5,11 @@ class ClassComponent extends React.Component {
   constructor() {
     super();
 
-    console.log('this is class component constructor 3');
+    // 这里为什么会执行两次？
+    // 可以发现，一旦文件发生修改，两次的结果会不一致，一次是老的，一次新的，且新的在前
+    // this is class component constructor 2
+    // this is class component constructor 1
+    console.log('this is class component constructor 2');
   }
   render() {
     return (
--- root.js
+++ root.js
@@ -1,8 +1,14 @@
 if (process.env.NODE_ENV !== 'production') {
   var hot = require('./index').hot;
   if (module.hot) {
+    //TODO: module 和 require 哪里来的
     var cache = require.cache;
 
+    console.log(module.parents);
+
+    // 这里的parents就是使用hot方法的文件路径
+    // './src/components/App.js'
+    // './src/components/Hook.js'
     if (!module.parents || module.parents.length === 0) {
       throw new Error(
         'React-Hot-Loader: `react-hot-loader/root` is not supported on your system. ' +
